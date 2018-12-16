07a7e416e3ceea0a09069146e81badbc5bc4696c
Axel Maldonado <axel@axelmaldonado.com>
2018-12-16 17:57:45 -0600

    finished Flicklist 0 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,27 +1,24 @@
-
-
 var api = {
-	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
-}
+  root: "https://api.themoviedb.org/3",
+  token: "e7f97a7595d99356ba2dbf15d9696923"
+};
 
 /**
  * Makes an AJAX request to themoviedb.org, asking for some movies
  * if successful, prints the results to the console
  */
 function testTheAPI() {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-		}
-	});
+  $.ajax({
+    url: api.root + "/discover/movie",
+    data: {
+      api_key: api.token
+    },
+    success: function(response) {
+      console.log("We got a response from The Movie DB!");
+      console.log(response);
+    }
+  });
 }
-
 
 console.log("The script loaded!");
 testTheAPI();
